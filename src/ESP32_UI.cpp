1488a15ff139577c1eb22b7e140d8d4d3ed2bf14
Reuben <silrep@emypeople.net>
2022-07-19 15:32:49 -0400

    remove ADS1115 options used for 12v and 3.3v error processing

--- src/ESP32_UI.cpp
+++ src/ESP32_UI.cpp
@@ -29,11 +29,7 @@ void setResetButtonToRed() {
 void addAnalogInputADS1115( uint16_t parent ) {
   ESPUI.addControl( ControlType::Option, "ADS1115 A0 Single", String( ( uint8_t )SteerConfig::AnalogIn::ADS1115A0Single ), ControlColor::Alizarin, parent );
   ESPUI.addControl( ControlType::Option, "ADS1115 A1 Single", String( ( uint8_t )SteerConfig::AnalogIn::ADS1115A1Single ), ControlColor::Alizarin, parent );
-  ESPUI.addControl( ControlType::Option, "ADS1115 A2 Single", String( ( uint8_t )SteerConfig::AnalogIn::ADS1115A2Single ), ControlColor::Alizarin, parent );
-  ESPUI.addControl( ControlType::Option, "ADS1115 A3 Single", String( ( uint8_t )SteerConfig::AnalogIn::ADS1115A3Single ), ControlColor::Alizarin, parent );
-  ESPUI.addControl( ControlType::Option, "ADS1115 A0 Differential", String( ( uint8_t )SteerConfig::AnalogIn::ADS1115A0A1Differential ), ControlColor::Alizarin, parent );
-  ESPUI.addControl( ControlType::Option, "ADS1115 A2 Differential", String( ( uint8_t )SteerConfig::AnalogIn::ADS1115A2A3Differential ), ControlColor::Alizarin, parent );
-}
+  ESPUI.addControl( ControlType::Option, "ADS1115 A0 Differential", String( ( uint8_t )SteerConfig::AnalogIn::ADS1115A0A1Differential ), ControlColor::Alizarin, parent );}
 
 void initESPUI ( void ) {
 
